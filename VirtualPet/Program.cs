2be9be8a7570093da605363a29275505b654935f
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-08-23 17:35:42 -0400

    fix connection to github

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VirtualPets marley = new VirtualPets("Marley", 25, 5, 6 );
+            Console.WriteLine("Welcome to Virtual Pets management Console");
+            Console.WriteLine("Please type Yes to manage {0} ?", marley.Name);
+            string userResponse = Console.ReadLine().ToLower();
+            while (userResponse.Equals("yes"))
+            {
+                Console.WriteLine("Please select from the following to manage your Virtual Pet {0} ", marley.Name);
+
+                Console.WriteLine("Select 1 to get the status of your Virtual Pet {0} ", marley.Name);
+                Console.WriteLine("Select 2 to feed {0} food", marley.Name);
+                Console.WriteLine("Select 3 to give {0} water", marley.Name);
+                Console.WriteLine("Select 4 to take {0} outside to go potty", marley.Name);
+                int userChoice = int.Parse(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 1:
+                    marley.Tick();
+                if (marley.HungerAlert)
+                {
+                    Console.WriteLine(" {0} is hungry {1} and needs to be fed some food.", marley.Name, marley.Hunger);
+                    Console.WriteLine("Please enter how much food you would like to give {0} ", marley.Name);
+                    int newHunger = int.Parse(Console.ReadLine());
+                    marley.SetHunger(newHunger);
+                }
+                if (marley.ThirstAlert)
+                {
+                    Console.WriteLine(" {0}  is dehydrated and needs water.", marley.Name);
+                    Console.WriteLine("Please enter how much water you want to give {0} ", marley.Name);
+                    marley.SetThirst(int.Parse(Console.ReadLine()));
+                }
+                if (marley.WasteAlert)
+                {
+                    Console.WriteLine(" {0} hasnt been outside in a long time and needs to potty.", marley.Name);
+                    Console.WriteLine("Please enter a new level of waste you want {0} to have left", marley.Name);
+                    Console.WriteLine(" {0} hasnt been outside in a long time and needs to potty.", marley.Name);
+                    marley.SetWaste(int.Parse(Console.ReadLine()));
+                }
+                    Console.WriteLine(" {0} now has a {1} hunger level and a {2} thirst level and a {3} waste level", marley.Name, marley.Hunger, marley.Thirst, marley.Waste);
+
+                break;
+                     case 2:
+                        marley.Tick();
+                        Console.WriteLine("Enter a new hunger level.");
+                        int hunger = int.Parse(Console.ReadLine());
+                        marley.SetHunger(hunger);
+                        Console.WriteLine("The new depth of the {0} river is {1}", marley.Name, marley.Hunger);
+                break;
+
+                     case 3:
+                        marley.Tick();
+                        Console.WriteLine("Enter a new thirst level.");
+                        int thirst = int.Parse(Console.ReadLine());
+                        marley.SetThirst(thirst);
+                         Console.WriteLine("The new thirst level of {0} is {1}", marley.Name, marley.Thirst);
+                break;
+                     case 4:
+                        marley.Tick();
+                        Console.WriteLine("Enter a new waste level.");
+                        int waste = int.Parse(Console.ReadLine());
+                        marley.SetWaste(waste);
+                        Console.WriteLine("The new waste level of {0} is {1}", marley.Name, marley.Waste);
+                break;
+                default:
+                break;
+                }
+            
+            }
+                    Console.WriteLine("Please type Yes to manage Virtual Pet {0} ?", marley.Name);
+                    userResponse = Console.ReadLine().ToLower();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ VirtualPet/VirtualPets.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPets
+    {// declare fields
+        private int hunger;
+        private int thirst;
+        private int waste;
+        private string name;
+        private bool hungerAlert;
+        private bool thirstAlert;
+        private bool wasteAlert;
+        //declare properties
+        public bool HungerAlert
+        {
+            get { return this.hungerAlert; }
+            set { this.hungerAlert = value; }
+        }
+        public bool ThirstAlert
+        {
+            get { return this.thirstAlert; }
+            set { this.thirstAlert = value; }
+        }
+        public bool WasteAlert
+        {
+            get { return this.wasteAlert; }
+            set { this.wasteAlert = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+        public int Waste
+        {
+            get { return this.waste; }
+            set { this.waste = value; }
+        }
+
+        //Create constructors
+
+        public VirtualPets()
+        {
+            //Default Costructor
+        }
+        public VirtualPets(string name, int hunger, int thirst, int waste)
+        {
+            this.Name = name;
+            this.Hunger = hunger;
+            this.Thirst = thirst;
+            this.Waste = waste;
+        }
+        //Create Methods
+        public int SetHunger(int num)
+        {
+            this.Hunger = num;
+            return Hunger;
+        }
+        public int SetThirst(int num)
+        {
+            this.Thirst = num;
+            return Thirst;
+        }
+        public int SetWaste(int num)
+        {
+            this.Waste = num;
+            return Waste;
+        }
+
+        public string SetName(string name)
+        {
+            this.Name = name;
+            return Name;
+        }
+
+        public void Tick()
+        {
+            Hunger++;
+            if (Hunger > 25)
+            {
+                HunAlert(true);
+            }
+            else
+            {
+                HunAlert(false);
+            }
+            Thirst--;
+            if (Thirst > 5)
+            {
+                ThirAlert(true);
+            }
+            else
+            {
+                ThirAlert(false);
+            }
+            Waste++;
+            if (Waste > 6)
+            {
+                WasAlert(true);
+            }
+            else
+            {
+                WasAlert(true);
+            }
+        }
+
+        public bool HunAlert(bool status)
+        {  
+                hungerAlert = status;
+                return hungerAlert;
+         }
+        public bool ThirAlert(bool status)
+        {
+                thirstAlert = status;
+                return thirstAlert;
+        }
+        public bool WasAlert(bool status)
+        {
+            wasteAlert = status;
+            return wasteAlert;
+        }
+
+
+
+
+
+    }
+}   
+
